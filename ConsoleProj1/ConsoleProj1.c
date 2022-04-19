271d03d2bb51e476f1a7974d9b906f23364af6b2
JeongJunSoo94 <103792742+JeongJunSoo94@users.noreply.github.com>
2022-04-19 12:28:10 +0900

    Add files via upload

---
+++ ConsoleProj1/ConsoleProj1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("%c\n", 'A');
+	printf("%s\n", "A");
+	printf("%c는 %s 입니다.\n", '1', "first");
+
+
+	printf("10진법 %d 는 ASCII %c 입니다\n", 32, 65);
+	return 0;
+}
